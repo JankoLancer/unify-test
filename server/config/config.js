6535efcd2b2eec9bbdde579bc85d6e7cfc71c0f2
Janko <janko.djuric22@gmail.com>
2019-04-21 23:22:58 +0200

    fix config port

--- server/config/config.js
+++ server/config/config.js
@@ -32,7 +32,7 @@ if (error) {
 
 const config = {
   env: envVars.NODE_ENV,
-  port: envVars.SERVER_PORT,
+  port: process.env.PORT || envVars.SERVER_PORT,
   mongooseDebug: envVars.MONGOOSE_DEBUG,
   jwtSecret: envVars.JWT_SECRET,
   frontend: envVars.MEAN_FRONTEND || 'angular',
